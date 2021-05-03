11f7278bbee151b90a9863731f1d09afec676150
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-05-03 15:16:58 +0300

    front&back

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const cors = require('cors'); // to access withou problms
+const data = require('./client/City-Explorer/src/data.json');
+const app = express()
+ app.use(cors());
+app.get('/', function (req, res) {
+  res.send('Hello Worldddddd')
+});
+// app.get('/about', function (req, res) {
+//     res.send('Hello World from about me')
+//   });
+app.listen(3001)
